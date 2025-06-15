5ca8fe87d0a2558d26af1f4c5399d748ffc81765
yuriiavr <yurii.avramets@gmail.com>
2025-06-15 18:23:50 +0300

    Deploy

---
+++ index.js
@@ -0,0 +1,2 @@
+import"./assets/styles-C95aE8-2.js";const o=document.querySelectorAll(".section-link");o.forEach(e=>{e.style.width="50%"});o.forEach(e=>{e.addEventListener("mouseenter",r=>{r.target.style.width="70%",o.forEach(t=>{t!==r.target&&(t.style.width="30%")})}),e.addEventListener("mouseleave",r=>{o.forEach(t=>{t.style.width="50%"})})});
+//# sourceMappingURL=index.js.map
